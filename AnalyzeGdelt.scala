fbd9bd6b8f18f98a493cf913d62c34753491ab8c
sorianoe <sorianoe@gmail.com>
2016-06-09 13:41:59 +0200

    Update AnalyzeGdelt.scala

--- AnalyzeGdelt.scala
+++ AnalyzeGdelt.scala
@@ -4,6 +4,7 @@ import org.apache.spark.graphx.{Graph, Edge, EdgeTriplet, GraphLoader}
 import org.apache.spark.{SparkContext, SparkConf}
 
 
+
 import scala.xml.Null
 
 /**
@@ -16,38 +17,34 @@ object AnalyzeGdelt {
 
     val sc = new SparkContext(conf)
 
-        //graphx solo soporta
-    //GraphX currently only supports integer vertex ids.
-    //conversion del tipo de dato string a entero
-
-    //“processed-gdelt”, que contiene la lista de aristas o arcos.
-    // Load the edges as a graph
-    //The file should only contain two integers in each line, a source id and a target id.
-
-    val graph = GraphLoader.edgeListFile(sc,"hdfs://localhost:8020/user/cloudera/processed-gdelt")
+      val graph = GraphLoader.edgeListFile(sc,"hdfs://localhost:8020/user/cloudera/processed-gdelt")
 
     // Run PageRank
     //PageRank para detectar relaciones
     val ranks = graph.pageRank(0.0001).vertices
 
-    // join the ids with the phone numbers
-    //val entities = sc.textFile("metadata-lookup-gdelt").map
+    // join the ids with the gdelt numbers
     val entities = sc.textFile("hdfs://localhost:8020/user/cloudera/lookup-gdelt").map
     {
       line =>val fields = line.split("\\s+")
         (fields(0).toLong, fields(1))
     }
 
-    val ranksByVertex = entities.join(ranks).map {
+    val ranksPorVertice = entities.join(ranks).map {
       case (id, (vertex, rank)) => (rank, vertex)
     }
-    // print out the top 5 entities
-    println(ranksByVertex.sortByKey(false).take(10).mkString("\n"))
+    // print out the top 10 entities
+    println(ranksPorVertice.sortByKey(false).take(10).mkString("\n"))
 
 
+    //Estadisticas GRAFO
+
+    val nvertices=graph.numVertices ;
+    println("Vertices Grafo:",nvertices);
+
+    val naristas= graph.numEdges;
+    println("Aristas Grafo:",naristas);
 
   }
-
-
 
 }
